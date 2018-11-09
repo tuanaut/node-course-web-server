5cf68796f0ac2a74dc89380cbba00ac083f5fc1b
tuanaut <tuan4444c@yahoo.com>
2018-11-08 20:40:08 -0800

    added partials and hbs files

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
 const express = require('express');
 const fs = require('fs');
+const hbs = require('hbs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // heroku creates environment port, if that doesn't exist
+// localhost:3000 still works
 
 var app  = express();
 
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials'); // partials so we don't have to maintain same code in all the templates
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+app.set('view engine', 'hbs'); //using hbs as the default view engine
+
+app.use(express.static(__dirname + '/public')); // makes it so we can render the file specified
+// in url. So now localhost:3000/help.html will take you to help.html page
 
 app.use((req,res,next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log,  (err) => {
-    if(err){
-      console.log('Unable to log');
-    }
-  });
 
-  next();
+  next(); // need next to move on from this middleware use
 });
 
+
+
 app.get('/', (req, res) => {
-  res.send({
-    name: 'Andrew',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  res.render('home.hbs',
+  {
+      pageTitle: 'Home',
+      welcomeMsg: 'Welcome Home'
   })
 });
 
+// /about renders about.hbs
 app.get('/about', (req, res) => {
-  res.render('about.hbs',
+  res.render('about.hbs', // rendering hbs because we set view engine to hbs earlier
   {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
+  // can pass to about.hbs by adding this object as second arg
+  })
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
   })
 });
 
